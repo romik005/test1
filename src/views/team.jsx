b145b3d6e2727b57df06ab93218160da577dad78
romik005 <84709991+romik005@users.noreply.github.com>
2021-06-01 11:02:53 +0530

    Update team.jsx

--- src/views/team.jsx
+++ src/views/team.jsx
@@ -11,9 +11,6 @@ function Form() {
 
         async function dataget(){
             const dd = await axios('http://134.209.103.120:5500/api/team');
-            
-           await fetch('http://134.209.103.120:5500/api/team') .then(response => response.json()) .then(data => console.log(data));
-//             console.log(dd);
             setTeam(dd.data);
         }
         dataget();
