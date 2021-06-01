c9226500165b5263315f56c2348478459ca28f1c
romik005 <84709991+romik005@users.noreply.github.com>
2021-06-01 17:48:44 +0530

    Update life.jsx

--- src/views/life.jsx
+++ src/views/life.jsx
@@ -25,7 +25,116 @@ function Career() {
                     </div>
                 </section>
 
-                 <section className="u-align-left u-clearfix u-section-1" id="sec-0fe2" style={{minHeight: 'fit-content'}}><div className="u-clearfix u-sheet u-sheet-1" style={{marginTop: '100px', marginBottom: '100px', minHeight: 'fit-content'}}><div className="u-clearfix u-expanded-width u-layout-wrap u-layout-wrap-1"><div className="u-layout"><div><div className="u-layout-row career_moniter"><div className="u-container-style u-layout-cell u-size-25"><div className="u-container-layout u-container-layout-1"><img alt="" className="life_images" src="http://134.209.103.120:5500/public/images/lifeimages-1621836280894.png" /></div></div><div className="u-container-style u-layout-cell u-size-10"><div className="u-container-layout u-container-layout-2 careerlife_date"><div className="life_date_show"><div className="life_title">26 Jan</div></div><div className="life_line_show" /></div></div><div className="u-container-style u-layout-cell u-size-25"><div className="u-container-layout u-container-layout-3"><div className="u-container-layout u-valign-middle u-container-layout-1"><h2 className="u-align-center u-custom-font u-text u-text-1 laravel_uiux_disk_title" style={{margin: '16px 0px 0px'}}>New Year celebration</h2><div className="laravel_uiux_border" style={{width: '-webkit-fill-available'}}><div style={{width: '80px', margin: 'auto', background: 'white'}}><i className="fas fa-diamond" /><i className="fas fa-diamond" /><i className="fas fa-diamond" /></div></div><div className="life_description" /></div></div></div></div><div className="career_phone"><h2 className="u-align-center u-custom-font u-text u-text-1 laravel_uiux_disk_title" style={{margin: '16px 0px 0px'}}>New Year celebration</h2><div className="laravel_uiux_border" style={{zIndex: 1, position: 'relative'}}><div style={{width: '80px', margin: 'auto', background: 'white', position: 'absolute', left: '0px', right: '0px'}}><i className="fas fa-diamond" /><i className="fas fa-diamond" /><i className="fas fa-diamond" /></div></div><div className="life_description" style={{textAlign: 'center', padding: '40px 0px'}} /><img alt="" className="life_images" src="http://134.209.103.120:5500/public/images/lifeimages-1621836280894.png" /></div></div><div><div className="u-layout-row career_moniter"><div className="u-container-style u-layout-cell u-size-25 u-container-layout-1"><div className="u-container-layout u-container-layout-1"><h2 className="u-align-center u-custom-font u-text u-text-1 laravel_uiux_disk_title" style={{margin: '16px 0px 0px'}}>Charity trust</h2><div className="laravel_uiux_border" style={{width: '-webkit-fill-available'}}><div style={{width: '80px', margin: 'auto', background: 'white'}}><i className="fas fa-diamond" /><i className="fas fa-diamond" /><i className="fas fa-diamond" /></div></div><div className="life_description" /></div></div><div className="u-container-style u-layout-cell u-size-10"><div className="u-container-layout u-container-layout-2 careerlife_date"><div className="life_date_show"><div className="life_title">26 Feb</div></div><div className="life_line_show" /></div></div><div className="u-container-style u-layout-cell u-size-25"><div className="u-container-layout u-container-layout-3"><div className="u-container-layout u-valign-middle "><img alt="" className="life_images" src="http://134.209.103.120:5500/public/images/lifeimages-1621835279608.png" /></div></div></div></div><div className="career_phone"><h2 className="u-align-center u-custom-font u-text u-text-1 laravel_uiux_disk_title" style={{margin: '16px 0px 0px'}}>Charity trust</h2><div className="laravel_uiux_border" style={{zIndex: 1, position: 'relative'}}><div style={{width: '80px', margin: 'auto', background: 'white', position: 'absolute', left: '0px', right: '0px'}}><i className="fas fa-diamond" /><i className="fas fa-diamond" /><i className="fas fa-diamond" /></div></div><div className="life_description" style={{textAlign: 'center', padding: '40px 0px'}} /><img alt="" className="life_images" src="http://134.209.103.120:5500/public/images/lifeimages-1621835279608.png" /></div></div><div><div className="u-layout-row career_moniter"><div className="u-container-style u-layout-cell u-size-25"><div className="u-container-layout u-container-layout-1"><img alt="" className="life_images" src="http://134.209.103.120:5500/public/images/lifeimages-1621835174098.png" /></div></div><div className="u-container-style u-layout-cell u-size-10"><div className="u-container-layout u-container-layout-2 careerlife_date"><div className="life_date_show"><div className="life_title">26 Mar</div></div></div></div><div className="u-container-style u-layout-cell u-size-25"><div className="u-container-layout u-container-layout-3"><div className="u-container-layout u-valign-middle u-container-layout-1"><h2 className="u-align-center u-custom-font u-text u-text-1 laravel_uiux_disk_title" style={{margin: '16px 0px 0px'}}>Formhouse</h2><div className="laravel_uiux_border" style={{width: '-webkit-fill-available'}}><div style={{width: '80px', margin: 'auto', background: 'white'}}><i className="fas fa-diamond" /><i className="fas fa-diamond" /><i className="fas fa-diamond" /></div></div><div className="life_description">We believe in offering better rewards than just cash to encourage our employees to do better.</div></div></div></div></div><div className="career_phone"><h2 className="u-align-center u-custom-font u-text u-text-1 laravel_uiux_disk_title" style={{margin: '16px 0px 0px'}}>Formhouse</h2><div className="laravel_uiux_border" style={{zIndex: 1, position: 'relative'}}><div style={{width: '80px', margin: 'auto', background: 'white', position: 'absolute', left: '0px', right: '0px'}}><i className="fas fa-diamond" /><i className="fas fa-diamond" /><i className="fas fa-diamond" /></div></div><div className="life_description" style={{textAlign: 'center', padding: '40px 0px'}}>We believe in offering better rewards than just cash to encourage our employees to do better.</div><img alt="" className="life_images" src="http://134.209.103.120:5500/public/images/lifeimages-1621835174098.png" /></div></div></div></div></div></section>
+                 <section className="u-align-left u-clearfix u-section-1" id="sec-0fe2">
+                    <div className="u-clearfix u-sheet u-sheet-1" style={{ marginTop: '100px',marginBottom:'100px' }}>
+                        <div className="u-clearfix u-expanded-width u-layout-wrap u-layout-wrap-1">
+                            <div class="u-layout">
+                               <div>
+                                  <div class="u-layout-row career_moniter">
+                                     <div class="u-container-style u-layout-cell u-size-25">
+                                        <div class="u-container-layout u-container-layout-1"><img alt="" class="life_images" src="http://134.209.103.120:5500/public/images/lifeimages-1621836280894.png"></div>
+                                     </div>
+                                     <div class="u-container-style u-layout-cell u-size-10">
+                                        <div class="u-container-layout u-container-layout-2 careerlife_date">
+                                           <div class="life_date_show">
+                                              <div class="life_title">26 Jan</div>
+                                           </div>
+                                           <div class="life_line_show"></div>
+                                        </div>
+                                     </div>
+                                     <div class="u-container-style u-layout-cell u-size-25">
+                                        <div class="u-container-layout u-container-layout-3">
+                                           <div class="u-container-layout u-valign-middle u-container-layout-1">
+                                              <h2 class="u-align-center u-custom-font u-text u-text-1 laravel_uiux_disk_title" style="margin: 16px 0px 0px;">New Year celebration</h2>
+                                              <div class="laravel_uiux_border" style="width: -webkit-fill-available;">
+                                                 <div style="width: 80px; margin: auto; background: white;"><i class="fas fa-diamond"></i><i class="fas fa-diamond"></i><i class="fas fa-diamond"></i></div>
+                                              </div>
+                                              <div class="life_description"></div>
+                                           </div>
+                                        </div>
+                                     </div>
+                                  </div>
+                                  <div class="career_phone">
+                                     <h2 class="u-align-center u-custom-font u-text u-text-1 laravel_uiux_disk_title" style="margin: 16px 0px 0px;">New Year celebration</h2>
+                                     <div class="laravel_uiux_border" style="z-index: 1; position: relative;">
+                                        <div style="width: 80px; margin: auto; background: white; position: absolute; left: 0px; right: 0px;"><i class="fas fa-diamond"></i><i class="fas fa-diamond"></i><i class="fas fa-diamond"></i></div>
+                                     </div>
+                                     <div class="life_description" style="text-align: center; padding: 40px 0px;"></div>
+                                     <img alt="" class="life_images" src="http://134.209.103.120:5500/public/images/lifeimages-1621836280894.png">
+                                  </div>
+                               </div>
+                               <div>
+                                  <div class="u-layout-row career_moniter">
+                                     <div class="u-container-style u-layout-cell u-size-25 u-container-layout-1">
+                                        <div class="u-container-layout u-container-layout-1">
+                                           <h2 class="u-align-center u-custom-font u-text u-text-1 laravel_uiux_disk_title" style="margin: 16px 0px 0px;">Charity trust</h2>
+                                           <div class="laravel_uiux_border" style="width: -webkit-fill-available;">
+                                              <div style="width: 80px; margin: auto; background: white;"><i class="fas fa-diamond"></i><i class="fas fa-diamond"></i><i class="fas fa-diamond"></i></div>
+                                           </div>
+                                           <div class="life_description"></div>
+                                        </div>
+                                     </div>
+                                     <div class="u-container-style u-layout-cell u-size-10">
+                                        <div class="u-container-layout u-container-layout-2 careerlife_date">
+                                           <div class="life_date_show">
+                                              <div class="life_title">26 Feb</div>
+                                           </div>
+                                           <div class="life_line_show"></div>
+                                        </div>
+                                     </div>
+                                     <div class="u-container-style u-layout-cell u-size-25">
+                                        <div class="u-container-layout u-container-layout-3">
+                                           <div class="u-container-layout u-valign-middle "><img alt="" class="life_images" src="http://134.209.103.120:5500/public/images/lifeimages-1621835279608.png"></div>
+                                        </div>
+                                     </div>
+                                  </div>
+                                  <div class="career_phone">
+                                     <h2 class="u-align-center u-custom-font u-text u-text-1 laravel_uiux_disk_title" style="margin: 16px 0px 0px;">Charity trust</h2>
+                                     <div class="laravel_uiux_border" style="z-index: 1; position: relative;">
+                                        <div style="width: 80px; margin: auto; background: white; position: absolute; left: 0px; right: 0px;"><i class="fas fa-diamond"></i><i class="fas fa-diamond"></i><i class="fas fa-diamond"></i></div>
+                                     </div>
+                                     <div class="life_description" style="text-align: center; padding: 40px 0px;"></div>
+                                     <img alt="" class="life_images" src="http://134.209.103.120:5500/public/images/lifeimages-1621835279608.png">
+                                  </div>
+                               </div>
+                               <div>
+                                  <div class="u-layout-row career_moniter">
+                                     <div class="u-container-style u-layout-cell u-size-25">
+                                        <div class="u-container-layout u-container-layout-1"><img alt="" class="life_images" src="http://134.209.103.120:5500/public/images/lifeimages-1621835174098.png"></div>
+                                     </div>
+                                     <div class="u-container-style u-layout-cell u-size-10">
+                                        <div class="u-container-layout u-container-layout-2 careerlife_date">
+                                           <div class="life_date_show">
+                                              <div class="life_title">26 Mar</div>
+                                           </div>
+                                        </div>
+                                     </div>
+                                     <div class="u-container-style u-layout-cell u-size-25">
+                                        <div class="u-container-layout u-container-layout-3">
+                                           <div class="u-container-layout u-valign-middle u-container-layout-1">
+                                              <h2 class="u-align-center u-custom-font u-text u-text-1 laravel_uiux_disk_title" style="margin: 16px 0px 0px;">Formhouse</h2>
+                                              <div class="laravel_uiux_border" style="width: -webkit-fill-available;">
+                                                 <div style="width: 80px; margin: auto; background: white;"><i class="fas fa-diamond"></i><i class="fas fa-diamond"></i><i class="fas fa-diamond"></i></div>
+                                              </div>
+                                              <div class="life_description">We believe in offering better rewards than just cash to encourage our employees to do better.</div>
+                                           </div>
+                                        </div>
+                                     </div>
+                                  </div>
+                                  <div class="career_phone">
+                                     <h2 class="u-align-center u-custom-font u-text u-text-1 laravel_uiux_disk_title" style="margin: 16px 0px 0px;">Formhouse</h2>
+                                     <div class="laravel_uiux_border" style="z-index: 1; position: relative;">
+                                        <div style="width: 80px; margin: auto; background: white; position: absolute; left: 0px; right: 0px;"><i class="fas fa-diamond"></i><i class="fas fa-diamond"></i><i class="fas fa-diamond"></i></div>
+                                     </div>
+                                     <div class="life_description" style="text-align: center; padding: 40px 0px;">We believe in offering better rewards than just cash to encourage our employees to do better.</div>
+                                     <img alt="" class="life_images" src="http://134.209.103.120:5500/public/images/lifeimages-1621835174098.png">
+                                  </div>
+                               </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                 
 
 //                 <section className="u-align-left u-clearfix u-section-1" id="sec-0fe2">
 //                     <div className="u-clearfix u-sheet u-sheet-1" style={{ marginTop: '100px',marginBottom:'100px' }}>
